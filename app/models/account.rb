c166ab5b17c807d928a26933dadee25756b95e0a
nix12 <mr.goodwin@outlook.com>
2018-12-11 10:30:38 -0500

    Added friendly URLs and user management

---
+++ app/models/account.rb
@@ -0,0 +1,6 @@
+class Account < ApplicationRecord
+	has_many :users
+	resourcify
+	
+	validates :name, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
+	resources :accounts		
+
   root to: 'activity#mine'
   get 'activity/mine'
   get 'activity/feed'
+
+	get 'user/me', to: 'users#me', as: 'my_settings'
+	patch 'users/update_me', to: 'users#update_me', as: 'update_my_settings'
+	get 'user/password', to: 'users#password', as: 'my_password'
+	patch 'user/update_password', to: 'users#update_password', as: 'my_update_password'
+
+	scope 'account', as: 'account' do
+		resources :users
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
